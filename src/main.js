8b03dd57331622b781aa38adac1f68a50db9f405
Brandon DeRosier <x@bdero.me>
2016-04-25 23:37:46 -0400

    Add pointerlock to camera and render call

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+const PointerLockControls = require('./engine/pointerlock.js');
+
 const stats = require('./utils/stats'),
   _ = require('lodash'),
   renderer = require('./engine/renderer');
 
+let scene, camera, controls;
+
 function initialize() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, 16/9, 1, 10000);
+  controls = new PointerLockControls(camera);
+
+
   const resizeMessage = _.throttle(
     () => console.log(
       `Renderer resized to ${window.innerWidth} x ${window.innerHeight}.`
@@ -10,17 +19,21 @@ function initialize() {
   );
   function onWindowResize() {
     resizeMessage();
-
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
   }
-
+  onWindowResize();
   window.addEventListener('resize', onWindowResize, false);
+
 
   update();
 }
 
 function update () {
   requestAnimationFrame(update);
+
+  renderer.render(scene, camera);
 
   stats.update();
 }
